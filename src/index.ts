f7d1e8c0a528cd8cb96e2b53dd8fae0c8d58d2bb
Shigma <shigma10826@gmail.com>
2025-01-09 15:12:28 +0800

    feat: basic support

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { Context, Logger, remove, z } from 'koishi'
+import { createHmac } from 'node:crypto'
+
+const LOGGER_NAME = 'logger:lark-webhook'
+
+export interface Config {
+  url: string
+  secret: string
+  title: string
+}
+
+export const Config: z<Config> = z.object({
+  url: z.string().role('link').description('Lark webhook URL'),
+  secret: z.string().role('secret').description('Lark webhook secret'),
+  title: z.string(),
+})
+
+export const inject = ['http']
+
+export function apply(ctx: Context, config: Config) {
+  const target: Logger.Target = {
+    colors: 0,
+    async record(record) {
+      if (record.name === LOGGER_NAME) return
+      if (record.type !== 'error' && record.type !== 'warn') return
+      try {
+        const timestamp = '' + Math.round(Date.now() / 1000)
+        const sign = createHmac('sha256', `${timestamp}\n${config.secret}`).digest('base64')
+        const response = await ctx.http.post(config.url, {
+          timestamp,
+          sign,
+          msg_type: 'interactive',
+          card: {
+            elements: [{
+              tag: 'markdown',
+              content: `\`\`\`\n${record.content}\n\`\`\``,
+            }],
+            header: {
+              template: record.type === 'error' ? 'red' : 'yellow',
+              title: config.title ? {
+                content: `${config.title} (${record.name})`,
+                tag: 'plain_text',
+              } : undefined,
+            },
+          },
+        })
+        if (response.code) {
+          ctx.logger(LOGGER_NAME).error(`[${response.code}] ${response.msg}`)
+        }
+      } catch (error) {
+        ctx.logger(LOGGER_NAME).error(error)
+      }
+    },
+  }
+
+  ctx.effect(() => {
+    Logger.targets.push(target)
+    return () => remove(Logger.targets, target)
+  })
+}
